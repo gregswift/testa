7114ce22c3e03f1c7cdfca6e3de4e79faa90f307
Greg Swift <gregswift@gmail.com>
2015-02-26 16:04:50 -0600

    initial

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 setup(
     name = 'testa',
     description = 'Module for testing fedwatch',
-    version = '0.5',
+    version = '0.6',
     license = 'LGPLv2+',
     py_modules = ['testa'],
     entry_points={
